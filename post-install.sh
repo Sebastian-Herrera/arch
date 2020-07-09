dcdd256ab70824307c6dccd7e440186382b9551b
Sebastian-Herrera <56986461+Sebastian-Herrera@users.noreply.github.com>
2020-07-09 04:58:30 -0500

    Update post-install.sh

--- post-install.sh
+++ post-install.sh
@@ -1,1 +1,31 @@
+#!/bin/sh
 
+timedatectl set-ntp true
+ln -sf /usr/share/zoneinfo/America/Bogota /etc/localtime
+hwclock --systohc
+
+sed -i 's/#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen
+locale-gen
+
+sed '/week 7/a\first_weekday   2\nfirst_workday   2' /usr/share/i18n/locales/en_US
+locale-gen
+
+echo "LANG=en-US.UTF-8" >> /etc/locale.conf
+echo "KEYMAP=es" >> /etc/vconsole.conf
+
+echo "herrera" >> /etc/hostname
+
+echo "127.0.0.0       localhost\n::1             localhost\n127.0.1.1       herrera.localdomain     herrera" >> /etc/hosts
+
+passwd
+
+useradd -m -G wheel herrera -c "Sebastian Herrera"
+passwd herrera
+sed -i 's/# %wheel ALL=(ALL) ALL/%wheel ALL=(ALL) ALL/' /etc/sudoers
+
+systemctl enable NetworkManager
+
+grub-install /dev/sda
+grub-mkconfig -o /boot/grub/grub.cfg
+
+exit
