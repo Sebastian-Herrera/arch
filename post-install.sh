bdfeb3d7b943b676ca2017e4a34c8287202daec6
Sebastian-Herrera <56986461+Sebastian-Herrera@users.noreply.github.com>
2020-07-09 18:59:46 -0500

    Update post-install.sh

--- post-install.sh
+++ post-install.sh
@@ -16,10 +16,10 @@ echo "herrera" >> /etc/hostname
 
 echo -e "127.0.0.0       localhost\n::1             localhost\n127.0.1.1       herrera.localdomain     herrera" >> /etc/hosts
 
-echo "0" | passwd --stdin
+echo "0:root" | chpasswd 
 
 useradd -m -G wheel herrera -c "Sebastian Herrera"
-echo "0" | passwd --stdin herrera
+echo "0:herrera" | chpasswd
 sed -i 's/# %wheel ALL=(ALL) ALL/%wheel ALL=(ALL) ALL/' /etc/sudoers
 
 systemctl enable NetworkManager
