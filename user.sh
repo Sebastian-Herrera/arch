7dae2219afe2ce0737bce917b96fb29e8e754b2d
Sebastian-Herrera <56986461+Sebastian-Herrera@users.noreply.github.com>
2020-07-09 22:31:12 -0500

    Update user.sh

--- user.sh
+++ user.sh
@@ -7,12 +7,12 @@ sudo pacman -Sy
 git clone https://aur.archlinux.org/yay.git
 (cd yay && makepkg -si --noconfirm)
 
-yay -S --noconfirm xdg-user-dirs zsh zsh-theme-powerlevel10k-git neofetch qtile alacritty google-chrome 
+yay -S --noconfirm xdg-user-dirs zsh zsh-theme-powerlevel10k-git neofetch qtile alacritty 
+#google-chrome 
 
 xdg-user-dirs-update
 
-(zsh &&
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" &&
-source ~/.zshrc &&
-chsh -s $(which zsh) &&
-echo 'source /usr/share/zsh-theme-powerlevel10k/powerlevel10k.zsh-theme' >>! ~/.zshrc)
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+source ~/.zshrc
+chsh -s $(which zsh)
+echo 'source /usr/share/zsh-theme-powerlevel10k/powerlevel10k.zsh-theme' >>! ~/.zshrc
