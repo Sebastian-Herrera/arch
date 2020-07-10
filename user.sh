3906c592161c828803ffd98839b3f444a0c3f019
Sebastian-Herrera <56986461+Sebastian-Herrera@users.noreply.github.com>
2020-07-10 16:04:26 -0500

    Update user.sh

--- user.sh
+++ user.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 echo "0" | sudo -S pacman -Syyu
 
-echo "Server = http://repo.archlinux.fr/$arch" | sudo tee -a /etc/pacman.conf
+echo 'Server = http://repo.archlinux.fr/$arch' | sudo tee -a /etc/pacman.conf
 sudo pacman -Sy
 
 git clone https://aur.archlinux.org/yay.git
