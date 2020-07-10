d89603c216f0b66c9a14e6ceeb9b9424133b2260
Sebastian-Herrera <56986461+Sebastian-Herrera@users.noreply.github.com>
2020-07-09 22:06:19 -0500

    Update user.sh

--- user.sh
+++ user.sh
@@ -5,4 +5,15 @@ echo "Server = http://repo.archlinux.fr/$arch" | sudo tee -a /etc/pacman.conf
 sudo pacman -Sy
 
 git clone https://aur.archlinux.org/yay.git
-makepkg -si --noconfirm
+(cd yay && makepkg -si --noconfirm)
+
+yay -S --noconfirm xdg-user-dirs zsh zsh-theme-powerlevel10k-git neofetch qtile alacritty google-chrome 
+
+xdg-user-dirs-update
+
+(zsh &&
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" &&
+source ~/.zshrc &&
+chsh -s $(which zsh) &&
+
+echo 'source /usr/share/zsh-theme-powerlevel10k/powerlevel10k.zsh-theme' >>! ~/.zshrc)
