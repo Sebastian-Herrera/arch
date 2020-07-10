fbf5154d3b992e06b20ad9c007ad107ed177c664
Sebastian-Herrera <56986461+Sebastian-Herrera@users.noreply.github.com>
2020-07-09 23:49:58 -0500

    Update user.sh

--- user.sh
+++ user.sh
@@ -17,6 +17,8 @@ source ~/.zshrc
 #chsh -s $(which zsh)
 echo 'source /usr/share/zsh-theme-powerlevel10k/powerlevel10k.zsh-theme' >>! ~/.zshrc
 
+yay -S nvidia-340xx nvidia-340xx-settings nvidia-340xx-utils opencl-nvidia-340xx
+
 mkdir -p ~/.config/qtile/
 cp /usr/share/doc/<qtile_dir>/default_config.py ~/.config/qtile/config.py
 echo -e "#!/bin/sh\nexec qtile" >> ~/.xinitrc
