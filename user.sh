7dce2543e17daecaf922f122c976cea7d641f3b3
Sebastian-Herrera <56986461+Sebastian-Herrera@users.noreply.github.com>
2020-07-10 14:56:12 -0500

    Update user.sh

--- user.sh
+++ user.sh
@@ -8,6 +8,12 @@ git clone https://aur.archlinux.org/yay.git
 (cd yay && makepkg -si --noconfirm)
 rm -rfv yay/
 
+yay -S --noconfirm nvidia-340xx nvidia-340xx-settings 
+#nvidia-340xx-utils
+#opencl-nvidia-340xx
+sed -i 's/MODULES=()/MODULES=(nvidia)/' /etc/mkinitcpio.conf
+sudo mkinitcpio -p linux 
+
 yay -S --noconfirm xdg-user-dirs neofetch qtile alacritty google-chrome
 #zsh-theme-powerlevel10k-git 
 
@@ -17,12 +23,6 @@ xdg-user-dirs-update
 #sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 #source ~/.zshrc
 #echo 'source /usr/share/zsh-theme-powerlevel10k/powerlevel10k.zsh-theme' >>! ~/.zshrc
-
-yay -S --noconfirm nvidia-340xx nvidia-340xx-settings 
-#nvidia-340xx-utils
-#opencl-nvidia-340xx
-sed -i 's/MODULES=()/MODULES=(nvidia)/' /etc/mkinitcpio.conf
-sudo mkinitcpio -p linux
 
 mkdir -p ~/.config/qtile/
 cp /usr/share/doc/<qtile_dir>/default_config.py ~/.config/qtile/config.py
