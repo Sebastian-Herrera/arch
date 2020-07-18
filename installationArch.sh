8297bcbde3fbbd2b0475f9910e4e6a151538a40c
Sebastian-Herrera <56986461+Sebastian-Herrera@users.noreply.github.com>
2020-07-18 02:00:02 -0500

    Update installationArch.sh

--- installationArch.sh
+++ installationArch.sh
@@ -13,7 +13,7 @@ reflector --sort rate -l 5 --save /etc/pacman.d/mirrorlist
 mount /dev/sda1 /mnt
 
 #pacstrap /mnt base sudo binutils make gcc pkgconf fakeroot linux-lts linux-firmware intel-ucode neovim grub networkmanager xorg-server xorg-xinit mesa mesa-demos git
-pacstrap /mnt base base-devel linux linux-firmware intel-ucode man-db man-pages grub networkmanager zsh neovim git xorg-server xorg-xinit mesa mesa-demos
+pacstrap /mnt base base-devel linux linux-firmware intel-ucode man-db man-pages grub networkmanager zsh neovim git xorg-server xorg-xinit mesa mesa-demos pulseaudio
 
 genfstab -U /mnt >> /mnt/etc/fstab
 
