55998984076071b685dd6b87916d33ff38262eeb
Sebastian-Herrera <56986461+Sebastian-Herrera@users.noreply.github.com>
2020-07-30 01:52:34 -0500

    Update user.sh

--- user.sh
+++ user.sh
@@ -23,7 +23,7 @@ echo '0' | sudo pacman -Sy --noconfirm xdg-user-dirs neofetch noto-fonts-emoji u
 curl -sS https://download.spotify.com/debian/pubkey.gpg | gpg --import - && gpg --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 931FF8E79F0876134EDDBDCCA87FF9DF48BF1C90
 yay -Sy --noconfirm systemd-numlockontty zsh-theme-powerlevel10k-git escrotum-git google-chrome visual-studio-code-bin spotify spicetify-cli
 #localectl set-locale LANG=en_US.UTF-8
-systemctl enable numLockOnTty
+echo '0' | sudo -S systemctl enable numLockOnTty
 
 xdg-user-dirs-update
 
