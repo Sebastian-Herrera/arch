f2bc4b9ccdbdf0696cb78322237ebce2c5e94374
Sebastian-Herrera <56986461+Sebastian-Herrera@users.noreply.github.com>
2020-07-30 01:37:43 -0500

    Update user.sh

--- user.sh
+++ user.sh
@@ -20,8 +20,8 @@ sudo mkinitcpio -p linux
 #sudo nvidia-xconfig
 
 echo '0' | sudo pacman -Sy --noconfirm xdg-user-dirs neofetch noto-fonts-emoji unzip qtile hsetroot gtk2 gtk3 alacritty rofi perl-file-mimeinfo ranger nautilus pulseaudio playerctl unrar fzf catimg redshift nodejs-lts-erbium npm ntfs-3g
-yay -Sy --noconfirm systemd-numlockontty zsh-theme-powerlevel10k-git escrotum-git google-chrome visual-studio-code-bin spotify spicetify-cli
 curl -sS https://download.spotify.com/debian/pubkey.gpg | gpg --import -
+yay -Sy --noconfirm systemd-numlockontty zsh-theme-powerlevel10k-git escrotum-git google-chrome visual-studio-code-bin spotify spicetify-cli
 #localectl set-locale LANG=en_US.UTF-8
 systemctl enable numLockOnTty
 
