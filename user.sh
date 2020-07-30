839525ba08470def98f783ba73733f84038ab586
Sebastian-Herrera <56986461+Sebastian-Herrera@users.noreply.github.com>
2020-07-30 16:49:27 -0500

    Update user.sh

--- user.sh
+++ user.sh
@@ -32,7 +32,7 @@ yay -Sy --noconfirm systemd-numlockontty zsh-theme-powerlevel10k-git escrotum-gi
 #localectl set-locale LANG=en_US.UTF-8
 echo '0' | sudo -S systemctl enable numLockOnTty
 
-echo 'source /usr/share/zsh-theme-powerlevel10k/powerlevel10k.zsh-theme' >> ~/.zshrc
+#echo 'source /usr/share/zsh-theme-powerlevel10k/powerlevel10k.zsh-theme' >> ~/.zshrc
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search && bindkey '^[[A' history-substring-search-up && bindkey '^[[B' history-substring-search-down
