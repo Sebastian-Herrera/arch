dde735ff417bbebdb3a9dcbe933901ceb25e2ca4
Sebastian Herrera <56986461+Sebastian-Herrera@users.noreply.github.com>
2020-10-25 01:16:03 -0500

    Update user.sh

--- user.sh
+++ user.sh
@@ -34,12 +34,12 @@ yay -Sy --noconfirm nautilus-open-any-terminal escrotum-git bottom google-chrome
 #localectl set-locale LANG=en_US.UTF-8
 
 #Starship
-git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting ~/.zsh/zsh-syntax-highlighting
-curl -O https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/plugins/archlinux/archlinux.plugin.zsh && mkdir ~/.zsh/archlinux/ && mv archlinux.plugin.zsh ~/.zsh/archlinux/
-curl -O https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/plugins/colored-man-pages/colored-man-pages.plugin.zsh && mkdir ~/.zsh/colored-man-pages/ && mv colored-man-pages.plugin.zsh ~/.zsh/colored-man-pages/
-curl -O https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/plugins/history-substring-search/history-substring-search.zsh && mkdir ~/.zsh/history-substring-search/ && mv history-substring-search.zsh ~/.zsh/history-substring-search/
-curl -O https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/plugins/git/git.plugin.zsh && mkdir ~/.zsh/git/ && mv git.plugin.zsh ~/.zsh/git/
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting ~/.zsh/plugins/zsh-syntax-highlighting
+curl -O https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/plugins/archlinux/archlinux.plugin.zsh && mkdir ~/.zsh/plugins/archlinux/ && mv archlinux.plugin.zsh ~/.zsh/archlinux/
+curl -O https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/plugins/colored-man-pages/colored-man-pages.plugin.zsh && mkdir ~/.zsh/plugins/colored-man-pages/ && mv colored-man-pages.plugin.zsh ~/.zsh/colored-man-pages/
+curl -O https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/plugins/history-substring-search/history-substring-search.zsh && mkdir ~/.zsh/plugins/history-substring-search/ && mv history-substring-search.zsh ~/.zsh/history-substring-search/
+curl -O https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/plugins/git/git.plugin.zsh && mkdir ~/.zsh/plugins/git/ && mv git.plugin.zsh ~/.zsh/git/
 
 #OHMYZSH
 #echo 'source /usr/share/zsh-theme-powerlevel10k/powerlevel10k.zsh-theme' >> ~/.zshrc
