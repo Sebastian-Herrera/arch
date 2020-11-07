1bb3c7b83e31327e14f6ce1885218fb24600778b
Sebastian Herrera <56986461+Sebastian-Herrera@users.noreply.github.com>
2020-11-06 21:21:53 -0500

    Update user.sh

--- user.sh
+++ user.sh
@@ -152,7 +152,7 @@ sudo unzip ngrok-stable-linux-amd64.zip -d /usr/local/bin/ && rm -fv ngrok-stabl
 
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/master/install.sh | zsh
 source ~/.zshrc
-nvm install --lts && sed -ie 's%$NVM_BIN%'"$NVM_BIN"'%g' ~/.stylelintrc.yml && sed -i 's/\/bin//g' ~/.stylelintrc.yml
+nvm install --lts && sed -ie 's%$USER%'"$USER"'%g' ~/.stylelintrc.yml && sed -ie 's%$NODE_VERSION%'"$(node -v)"'%g' ~/.stylelintrc.yml
 
 git config --global core.editor nvim
 
