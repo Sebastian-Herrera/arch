ff04c8f7de56d3ba29b641efd205e373f61b82eb
Sebastian Herrera <56986461+Sebastian-Herrera@users.noreply.github.com>
2020-11-13 15:01:01 -0500

    Update post-install.sh

--- post-install.sh
+++ post-install.sh
@@ -16,7 +16,7 @@ echo "KEYMAP=es" > /etc/vconsole.conf
 
 echo "herrera" > /etc/hostname
 
-echo -e "127.0.0.0\tlocalhost\n::1\t\tlocalhost\n127.0.1.1\tlocalhost" >> /etc/hosts
+echo -e "127.0.0.1\tlocalhost\n::1\t\tlocalhost\n127.0.1.1\therrera.localdomain\therrera" >> /etc/hosts
 
 echo "root:0" | chpasswd 
 
